f0fab18891daa4c5341f8988b4179c5e3b0c9a15
annnnny <annnnnyjj@gmail.com>
2015-08-22 13:05:40 +0800

    Initial Commit

---
+++ CatToday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CatToday
+//
+//  Created by HsuAnny on 8/22/15.
+//  Copyright (c) 2015 HackUC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CatToday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CatToday
+//
+//  Created by HsuAnny on 8/22/15.
+//  Copyright (c) 2015 HackUC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
